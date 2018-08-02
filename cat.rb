cc0031856452504a1518e09a15a498c923253266
gumpen <shuhei1993727@gmail.com>
2018-08-02 17:34:59 +0900

    Add option '-n' to 'cat'

--- cat.rb
+++ cat.rb
@@ -1,8 +1,23 @@
-def cat(files)
-  files.each do |file|
-    f = File.open(file,"r")
-    puts f.read
+require './common'
+
+def cat(files,options)
+  if options.include?("-n")
+    files.each do |file|
+      f = File.open(file,"r")
+      f.each_line do |line|
+        puts "#{f.lineno}: #{line}"
+      end
+    end
+  else
+    files.each do |file|
+      f = File.open(file,"r")
+      puts f.read
+    end
   end
 end
 
-cat(ARGV)
+
+args = ARGV
+options = extract_option_from_argument(args)
+files = args - options
+cat(files,options)
---
+++ common.rb
@@ -0,0 +1,3 @@
+def extract_option_from_argument(args)
+  args.grep(/^-/)
+end
